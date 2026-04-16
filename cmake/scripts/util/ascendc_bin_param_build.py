e013eed01bb4b9baff4c957bd1e7bb1e2ad12246
yayahello <zhaopenglei@hisilicon.com>
2026-04-16 11:30:15 +0800

    删除多余打印
    Co-authored-by: yayahello<zhaopenglei@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4081 merge 9.0.0 into 9.0.0
    
    删除多余打印
    
    Created-by: yayahello
    Commit-by: yayahello
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    编译时bisheng_flag刷屏，删除多余DFX代码
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    关联Issue [#1834](https://gitcode.com/cann/ops-transformer/issues/1834) 
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!4081

--- cmake/scripts/util/ascendc_bin_param_build.py
+++ cmake/scripts/util/ascendc_bin_param_build.py
@@ -404,8 +404,6 @@ grep -q \"None of the given tiling keys are in the supported list\"; then\n"
             build_cmd_var += f'export ASCEND_CUSTOM_OPP_PATH={auto_gen_path}:$ASCEND_CUSTOM_OPP_PATH \n'
         build_cmd_var += bin_cmd_str.format(fun=self.op_intf, soc=hard_soc, param=param_file,
                                            impl='high_performance,optional')
-        
-        print(f"bisheng_flags is: {bisheng_flags}")
 
         if bisheng_flags:
             # 如果 bisheng_flags 非空，直接使用其值
