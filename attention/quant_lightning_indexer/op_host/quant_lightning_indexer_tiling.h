7f24e1efb54a53bfbb9ebc4a13d26479383eddd9
wangyinchu1 <wangyinchu1@huawei.com>
2026-04-21 16:29:05 +0800

    QLI N1=8拦截放开
    Co-authored-by: wangyinchu<wangyinchu1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4393 merge QLIN1=8 into master
    
    QLI N1=8拦截放开
    
    Created-by: wangyinchu1
    Commit-by: wangyinchu
    Merged-by: cann-robot
    Description: ## 描述
    A5 QLI 支持N1=8，放开tiling侧对N1=8的拦截
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-transformer/issues/1985
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!4393

--- attention/quant_lightning_indexer/op_host/quant_lightning_indexer_tiling.h
+++ attention/quant_lightning_indexer/op_host/quant_lightning_indexer_tiling.h
@@ -79,7 +79,8 @@ constexpr uint32_t SPARSE_LIMIT = 2048;
 constexpr uint32_t G_SIZE_LIMIT = 64;
 constexpr uint32_t G_SIZE_LIMIT_950 = 24;
 constexpr uint32_t G_SIZE_LIMIT_32_950 = 32; 
-constexpr uint32_t G_SIZE_LIMIT_16_950 = 16; // ascend950只支持 G = 16/24/32/64
+constexpr uint32_t G_SIZE_LIMIT_16_950 = 16; // ascend950只支持 G = 8/16/24/32/64
+constexpr uint32_t G_SIZE_LIMIT_8_950 = 8;
 constexpr uint32_t BLOCK_SIZE_LIMIT = 1024;
 constexpr uint32_t BLOCK_SIZE_FACTOR = 16;
 constexpr uint32_t SPARSE_MODE_LOWER = 3;
