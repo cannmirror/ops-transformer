cc96ed0356b59094e2d821f55e57a86c2928c083
kdy18482276080 <kongdeyuan3@huawei.com>
2026-05-06 18:34:21 +0800

    新增融合算子mhc_pre_sinkhorn_backward
    Co-authored-by: kdy18482276080<kongdeyuan3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4760 merge mhcmhc into master
    
    新增融合算子mhc_pre_sinkhorn_backward
    
    Created-by: kdy18482276080
    Commit-by: kdy18482276080
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    新增融合算子mhc_pre_sinkhorn_backward
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-transformer/issues/2109
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    编译验证通过、冒烟验证通过、基础case验证通过。
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [x] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!4760

---
+++ mhc/mhc_pre_sinkhorn_backward/op_host/op_tiling/arch32/mhc_pre_sinkhorn_backward_tiling.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file mhc_pre_sinkhorn_backward_tiling.h
+ * \brief MhcPreSinkhornBackward operator tiling data definition
+ */
+#ifndef OP_HOST_OP_TILING_ARCH32_MHC_PRE_SINKHORN_BACKWARD_TILING_H
+#define OP_HOST_OP_TILING_ARCH32_MHC_PRE_SINKHORN_BACKWARD_TILING_H
+#include <tiling/tiling_api.h>
+#include "register/tilingdata_base.h"
+#include "tiling_base/tiling_base.h"
+#include "err/ops_err.h"
+
+namespace optiling {
+BEGIN_TILING_DATA_DEF(MhcPreSinkhornBackwardTilingData)
+TILING_DATA_FIELD_DEF(int64_t, batchSize)
+TILING_DATA_FIELD_DEF(int64_t, seqLength)
+TILING_DATA_FIELD_DEF(int64_t, c)
+TILING_DATA_FIELD_DEF(int64_t, n)
+TILING_DATA_FIELD_DEF(int64_t, c0)      // tile for c
+TILING_DATA_FIELD_DEF(int64_t, c1)      // tile count of c
+TILING_DATA_FIELD_DEF(int64_t, cTail)      // tail of c
+TILING_DATA_FIELD_DEF(int64_t, aivNum)      // tile count of c
+TILING_DATA_FIELD_DEF(int64_t, tileGradY)
+TILING_DATA_FIELD_DEF(int64_t, tileHHat2)
+TILING_DATA_FIELD_DEF(int64_t, tileSize)
+TILING_DATA_FIELD_DEF(int64_t, skIterCount)
+TILING_DATA_FIELD_DEF(int64_t, ubSize)
+TILING_DATA_FIELD_DEF(float, eps)
+
+TILING_DATA_FIELD_DEF_STRUCT(TCubeTiling, mm1TilingData)
+TILING_DATA_FIELD_DEF_STRUCT(TCubeTiling, mm2TilingData)
+END_TILING_DATA_DEF
+
+REGISTER_TILING_DATA_CLASS(MhcPreSinkhornBackward, MhcPreSinkhornBackwardTilingData)
+} // namespace optiling
+
+#endif // OP_HOST_OP_TILING_ARCH32_MHC_PRE_SINKHORN_BACKWARD_TILING_H
