135fa1492a52158e6b9390157dabd2b8f6907666
zhu-mingzhe71 <zhumingzhe2@huawei.com>
2026-05-18 15:51:33 +0800

    为AllGatherMatmul和AllGatherMatmulV2算子增加st用例
    Co-authored-by: zhu-mingzhe71<zhumingzhe2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5349 merge add_st into master
    
    为AllGatherMatmul和AllGatherMatmulV2算子增加st用例
    
    Created-by: zhu-mingzhe71
    Commit-by: zhu-mingzhe71
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    为AllGatherMatmul算子和AllGatherMatmulV2算子增加st用例，现在代码合入会有影响算子精度与功能的风险。增加基础场景与A8W8场景的用例。
    分别看护了：
    - 基础场景BF16与FP16输入
    - A8W8场景，perchannel与pertoken两种量化方式
    后续会依据新增特性继续新增看护用例
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-transformer/issues/2496
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [x] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!5349

--- mc2/all_gather_matmul/tests/st/aclnnAllGatherMatmul/executor_aclnnAllGatherMatmul.py
+++ mc2/all_gather_matmul/tests/st/aclnnAllGatherMatmul/executor_aclnnAllGatherMatmul.py
@@ -10,22 +10,26 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # -----------------------------------------------------------------------------------------------------------
 
-import math
 import torch
+import numpy as np
+import collections
 import torch.distributed as dist
-try:
-   import torch_npu
-except ImportError:
-   pass
+import torch_npu
 
+import ctypes
 from atk.configs.dataset_config import InputDataset
+from atk.configs.results_config import TaskResult
 from atk.tasks.api_execute import register
 from atk.tasks.api_execute.base_api import BaseApi
+from atk.tasks.api_execute.aclnn_base_api import AclnnBaseApi
+from atk.tasks.dataset.base_dataset import OpsDataset
+from atk.tasks.backends.lib_interface.acl_wrapper import AclTensor
+from atk.tasks.backends.lib_interface.acl_wrapper import AclFormat
 
-@register("function_aclnn_allgather_matmul")
-class DistFunctionApi(BaseApi):
-    def __init__(self, task_result):
-        super(DistFunctionApi, self).__init__(task_result)
+@register("function_allgather_matmul")
+class AllGatherMatmul(BaseApi):
+    def __init__(self, task_result: TaskResult):
+        super(AllGatherMatmul, self).__init__(task_result)
         self.dist_task_info = task_result.dist_task_info
 
     def __call__(self, input_data: InputDataset, with_output: bool = False):
@@ -33,16 +37,12 @@ class DistFunctionApi(BaseApi):
         world_size = self.dist_task_info.world_size
         input_chunk = input_data.kwargs['x1']
         weight_chunk = input_data.kwargs['x2']
-        gather_output = input_data.kwargs['gather_output']
 
         m_size = input_chunk.shape[0] // world_size
         if m_size != 0:
             start = self.dist_task_info.rank * m_size
-            end = start + m_size
+            end = start + m_size 
             input_chunk = input_chunk[start:end,:]
-
-        if weight_chunk.shape[0]!=input_chunk.shape[1] and weight_chunk.shape[1] == input_chunk.shape[1]:
-            weight_chunk = weight_chunk.transpose(0, 1)
 
         if input_chunk.shape == []:
             if self.name == "cpu" or self.dist_task_info.is_bm:
@@ -55,7 +55,7 @@ class DistFunctionApi(BaseApi):
             dist.all_gather(all_gather_out, input_chunk)
             all_gather_out = torch.cat(all_gather_out)
             output = torch.matmul(all_gather_out, weight_chunk)
-            gather_output_cpu = all_gather_out if gather_output else None
+            gather_output_cpu = all_gather_out
             return output, gather_output_cpu
 
         if self.dist_task_info.is_bm:
@@ -63,24 +63,55 @@ class DistFunctionApi(BaseApi):
             all_gather_out = torch.zeros(tensor_allgather_shape, dtype=input_chunk.dtype).npu()
             dist._all_gather_base(all_gather_out, input_chunk)
             output = torch.matmul(all_gather_out, weight_chunk)
-            gather_output_cpu = all_gather_out if gather_output else None
+            gather_output_cpu = all_gather_out
             return output, gather_output_cpu
 
+    def init_by_input_data(self, input_data: InputDataset):
+        OpsDataset.seed_everything()
+        input_chunk = input_data.kwargs['x1']
+        weight_chunk = input_data.kwargs['x2']
+
+        if weight_chunk.shape[0]!=input_chunk.shape[1] and weight_chunk.shape[1] == input_chunk.shape[1]:
+            input_data.kwargs['x2'] = input_data.kwargs['x2'].transpose(0, 1)
+        
+        if self.device == 'pyaclnn' and dist.is_available():
+            rank_id = self.dist_task_info.rank
+            input_data.kwargs['group'] = AllGatherMatmul.get_hcomm_info(rank_id)
+    
+    @staticmethod
+    def get_hcomm_info(rank_id):
+        from torch.distributed.distributed_c10d import _get_default_group
+        default_pg = _get_default_group()
+        if torch.__version__ > '2.0.1':
+            hcomm_info = default_pg._get_backend(torch.device("npu")).get_hccl_comm_name(rank_id)
         else:
-            if dist.is_available():
-                from torch.distributed.distributed_c10d import _get_default_group
-                default_pg = _get_default_group()
-                if torch.__version__ > '2.0.1':
-                    hcomm_info = default_pg._get_backend(torch.device("npu")).get_hccl_comm_name(rank_id)
-                else:
-                    hcomm_info = default_pg.get_hccl_comm_name(rank_id)
-            if self.task_result.case_config.id % 5 == 4 and 'bias' in input_data.kwargs:
-                bias = input_data.kwargs['bias']
-            else:
-                bias = None
+            hcomm_info = default_pg.get_hccl_comm_name(rank_id)
+        return hcomm_info
 
-            output = torch_npu.npu_all_gather_base_mm(input_chunk, weight_chunk,
-                                                      hcomm_info, world_size, gather_output=gather_output)
-            gather_output_npu = output[1] if gather_output else None
-            return output[0], gather_output_npu
 
+@register("aclnn_allgather_matmul")
+class AclnnAllGatherMatmul(AclnnBaseApi):
+    def __init__(self, task_result: TaskResult, backend):
+        super(AclnnAllGatherMatmul, self).__init__(task_result, backend)
+        self.dist_task_info = task_result.dist_task_info
+    
+    def init_by_input_data(self, input_data: InputDataset):
+
+        world_size = self.dist_task_info.world_size
+        input_chunk = input_data.kwargs['x1']
+        m_size = input_chunk.shape[0] // world_size
+        if m_size != 0:
+            start = self.dist_task_info.rank * m_size
+            end = start + m_size 
+            input_data.kwargs['x1'] = input_data.kwargs['x1'][start:end,:]
+        
+
+        input_args, output_packages = super().init_by_input_data(input_data)
+        
+        return input_args, output_packages
+
+    def get_null_tensor_pte(self):
+        acl_tensor_ptr = ctypes.POINTER(AclTensor)
+        null_void_ptr = ctypes.c_void_p(None)
+        null_tensor_ptr = ctypes.cast(null_void_ptr, acl_tensor_ptr)
+        return null_tensor_ptr
