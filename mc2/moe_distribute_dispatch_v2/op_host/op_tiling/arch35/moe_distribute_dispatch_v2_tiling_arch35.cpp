382f0a06d5e0cfd5c379eb1a23f1309abba512d9
gitcode_lijd <lijiandong20@huawei.com>
2026-07-03 16:19:00 +0800

    Revert D&C fusionpass
    Co-authored-by: gitcode_lijd<lijiandong20@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !7548 merge fix_2 into 9.1.0
    
    Revert D&C fusionpass
    
    Created-by: gitcode_lijd
    Commit-by: gitcode_lijd
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!7548

--- mc2/moe_distribute_dispatch_v2/op_host/op_tiling/arch35/moe_distribute_dispatch_v2_tiling_arch35.cpp
+++ mc2/moe_distribute_dispatch_v2/op_host/op_tiling/arch35/moe_distribute_dispatch_v2_tiling_arch35.cpp
@@ -1281,6 +1281,13 @@ ge::graphStatus MoeDistributeDispatchV2TilingFuncA5::CheckQuantModePtr(
     return ge::GRAPH_SUCCESS;
 }
 
+struct MoeDistributeDispatchCompileInfo {};
+static ge::graphStatus TilingParseForMoeDistributeDispatchV2(gert::TilingParseContext *context)
+{
+    (void)context;
+    return ge::GRAPH_SUCCESS;
+}
+
 static ge::graphStatus MoeDistributeDispatchV2TilingFuncImpl(gert::TilingContext* context)
 {
     MoeDistributeDispatchV2TilingFuncA5 impl;
@@ -1288,5 +1295,6 @@ static ge::graphStatus MoeDistributeDispatchV2TilingFuncImpl(gert::TilingContext
 }
 
 IMPL_OP_OPTILING(MoeDistributeDispatchV2)
-    .Tiling(MoeDistributeDispatchV2TilingFuncImpl);
+    .Tiling(MoeDistributeDispatchV2TilingFuncImpl)
+    .TilingParse<MoeDistributeDispatchCompileInfo>(TilingParseForMoeDistributeDispatchV2);
 } // namespace optiling
